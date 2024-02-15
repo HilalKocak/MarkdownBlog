ba5432a5dd05ad7c33a2ee04dcc4e1f4cd98f8ef
Hilal38 <hhilalkocak@gmail.com>
2024-02-15 15:14:37 +0300

    Use bootstrap to create views

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const app = express()
+const articleRouter = require('./routes/articles')
 
 app.set('view engine', 'ejs');
+
+app.use('/articles', articleRouter)
 
 
 app.get('/', (req, res)=> {
     // res.send('Hello World')
-    res.render('index') 
+    const articles = [{
+        title: 'Test Article',
+        createdAt: new Date(),
+        description: 'Test Description',
+    },
+    {
+        title: 'Test Article 2',
+        createdAt: new Date(),
+        description: 'Test Description',
+    },
+]
+    res.render('index', {articles:articles}) 
 })
 
 app.listen(5000)

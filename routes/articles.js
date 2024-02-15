36da0f6e987beb84a995c8f29f38a38e62adcb58
Hilal38 <hhilalkocak@gmail.com>
2024-02-15 17:26:40 +0300

    Post article and redirect detail

--- routes/articles.js
+++ routes/articles.js
@@ -6,12 +6,14 @@ router.get('/new', (req, res) => {
     res.render('articles/new', { article: new Article() })
 })
 
-router.get('/:id', (req, res) => {
-    // res.render(`/articles/${article.id}`)
+router.get('/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    if(article == null) res.redirect('/')
+    res.render('articles/show', {article:article}) // show.ejs
 })
 
 router.post('/', async (req, res) => {
-    const article = new Article({
+    let article = new Article({
         title: req.body.title,
         description:req.body.description,
         markdown: req.body.markdown
@@ -20,6 +22,7 @@ router.post('/', async (req, res) => {
         article = await article.save()
         res.redirect(`/articles/${article.id}`)
     }catch(e){
+        console.log('ERROR' , e)
         res.render('articles/new', {article: article}) 
     }
     

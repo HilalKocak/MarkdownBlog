86ddb77e861b930f0947e5acde2193a698586e9b
Hilal38 <hhilalkocak@gmail.com>
2024-02-15 18:47:23 +0300

    Implement delete article

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express()
 const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const Article = require('./models/article')
-
+const methodOverride = require('method-override')
 mongoose.connect('mongodb://localhost/blog')
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}))
-
+app.use(methodOverride('_method'))
 
 app.get('/', async (req, res)=> {
     // res.send('Hello World')

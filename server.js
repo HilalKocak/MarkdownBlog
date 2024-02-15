700d768b5924abb2314f8731da3157ec7e393654
Hilal38 <hhilalkocak@gmail.com>
2024-02-15 17:29:35 +0300

    Get all articles from mongodb

--- server.js
+++ server.js
@@ -2,28 +2,17 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
+const Article = require('./models/article')
 
 mongoose.connect('mongodb://localhost/blog')
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}))
 
 
-
-app.get('/', (req, res)=> {
+app.get('/', async (req, res)=> {
     // res.send('Hello World')
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test Description'
-
-    },
-    {
-        title: 'Test Article 2',
-        createdAt: new Date(),
-        description: 'Test Description',
-    }
-]
-  res.render('articles/index', {articles : articles})
+    const articles = await Article.find() // gets every single article
+    res.render('articles/index', {articles : articles})
 })
 app.use('/articles', articleRouter)
 app.listen(5000)

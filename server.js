2ba5c26efcf5dae4f889c6eba563158cba5982f0
Hilal38 <hhilalkocak@gmail.com>
2024-02-15 17:03:50 +0300

    Add cancel button

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const articleRouter = require('./routes/articles')
 
 mongoose.connect('mongodb://localhost/blog')
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}))
 
-app.use('/articles', articleRouter)
 
 
 app.get('/', (req, res)=> {
@@ -14,15 +14,16 @@ app.get('/', (req, res)=> {
     const articles = [{
         title: 'Test Article',
         createdAt: new Date(),
-        description: 'Test Description',
+        description: 'Test Description'
+
     },
     {
         title: 'Test Article 2',
         createdAt: new Date(),
         description: 'Test Description',
-    },
+    }
 ]
-    res.render('articles/index', {articles:articles}) 
+  res.render('articles/index', {articles : articles})
 })
-
+app.use('/articles', articleRouter)
 app.listen(5000)
